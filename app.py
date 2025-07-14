17883214605117278a821859f24ae553e1b713b8
ali-ari <alieatspizza@gmail.com>
2025-07-13 22:56:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -116,4 +116,5 @@ def index():
     return render_template('index.html', rembg_available=rembg_available)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
