6d0cae262901307abe50de3c8fa6e0626407ba6c
ali-ari <alieatspizza@gmail.com>
2025-07-13 23:11:12 -0700

    Fix: bind to Railway-provided port

--- app.py
+++ app.py
@@ -116,5 +116,6 @@ def index():
     return render_template('index.html', rembg_available=rembg_available)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000)) 
     app.run(host='0.0.0.0', port=port)
+    
